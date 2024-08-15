ec6ff6851e43daf5e77403445c14508e1f1ddd4b
Leonardo Miranda <leoamiranda2@gmail.com>
2024-08-15 15:38:04 -0300

    feat: peer dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,10 +2,13 @@ import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
+import tailwindcss from 'tailwindcss';
 
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
+
+import tailwindConfig from './tailwind.config.js';
 
 export default {
   input: 'src/widget/index.tsx',
@@ -27,7 +30,17 @@ export default {
     resolve({ browser: true }),
     commonjs(),
     typescript({ tsconfig: './tsconfig.json' }),
-    postcss(),
+    postcss({
+      config: {
+        path: './postcss.config.js',
+      },
+      extensions: ['.css'],
+      minimize: true,
+      inject: {
+        insertAt: 'top',
+      },
+      plugins: [tailwindcss(tailwindConfig)],
+    }),
     terser(),
     json(),
   ],
