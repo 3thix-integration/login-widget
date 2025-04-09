0410ee9768e29feb3b646a1d19e10e5dab5ad138
leomirandadev <leoamiranda2@gmail.com>
2025-04-09 00:04:22 -0300

    feat: implement token management and SSO handling in LoginWidget

--- src/widget/index.tsx
+++ src/widget/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
+import { deleteTokenFromURL, getTokenFromURL } from "./utils";
 
 type Props = {
   target: "PRODUCTION" | "SANDBOX";
   callback: (token: string) => void;
 };
+
+const token = getTokenFromURL();
 
 const productionUrl = "https://login.3thix.com/";
 const sandboxUrl = "https://sandbox-login.3thix.com/";
@@ -11,6 +14,7 @@ const sandboxUrl = "https://sandbox-login.3thix.com/";
 const LoginWidget = ({ callback, target }: Props) => {
   // const url = "http://localhost:3001";
   const url = target === "PRODUCTION" ? productionUrl : sandboxUrl;
+  const urlWithToken = !!token ? `${url}?token=${token}` : url;
 
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
@@ -18,8 +22,16 @@ const LoginWidget = ({ callback, target }: Props) => {
 
       if (event.data?.token) {
         callback(event.data.token);
+        deleteTokenFromURL();
+        return;
       }
-      // TODO: implement sso login from url
+
+      if (event.data?.ssoUrl) {
+        const url = new URL(event.data.ssoUrl);
+        url.searchParams.append("callbackUrl", window.location.href);
+        window.location.replace(url.href);
+        return;
+      }
     };
 
     window.addEventListener("message", handleMessage);
@@ -28,7 +40,7 @@ const LoginWidget = ({ callback, target }: Props) => {
 
   return (
     <iframe
-      src={url}
+      src={urlWithToken}
       title="Ethix Login"
       style={{
         width: "100%",
